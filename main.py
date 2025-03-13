ab7fc5cd89bed4d7796ae049340f1f67d84c963d
Rushabh Urkude <rru11092002@gmail.com>
2025-03-13 23:47:37 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,87 @@
+import pyttsx3 #!pip install pyttsx3
+import speech_recognition as sr
+import datetime
+import time
+
+def initialize_engine():
+    engine = pyttsx3.init("sapi5")
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', rate-50)
+    volume = engine.getProperty('volume')
+    engine.setProperty('volume', volume+0.25)
+    return engine
+
+def speak(text):
+    engine = initialize_engine()
+    engine.say(text)
+    engine.runAndWait()
+
+def command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source, duration=0.5)
+        print("Listening...")
+        r.pause_threshold = 1.0
+        r.phrase_threshold = 0.3
+        r.sample_rate = 48000
+        r.dynamic_energy_threshold = True
+        r.operation_timeout = 5
+        r.non_speaking_duration = 0.5
+        r.dynamic_energy_adjustment=2
+        r.energy_threshold = 4000
+        r.pause_time_limit = 10
+        # print(sr.Microphone.list_microphone_names()) 
+        audio = r.listen(source)
+        
+    try:
+        print("\r" , end="", flush=True)
+        print("Recognizing...", end="", flush=True)
+        query = r.recognize_google(audio, language='en-in')
+        print("\r" , end="", flush=True)
+        print(f"User said: {query}\n")
+    except Exception as e:
+        print("Say that again please...")
+        return "None"
+    return query
+
+def cal_day():
+    day = datetime.datetime.today().weekday() + 1
+    day_dict = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday', 7: 'Sunday'}
+    if day in day_dict.keys():
+        day_of_the_week = day_dict[day]
+        print(day_of_the_week)
+    return day_of_the_week
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    t = time.strftime("%I:%M:%p")
+    day = cal_day()
+    if (hour>=0) and (hour<12) and ("AM" in t):
+        speak("Good Morning! Rushabh, it's {day} and the time is {t}")
+    elif (hour>=12) and (hour<18) and ("PM" in t):
+        speak("Good Afternoon! Rushabh, it's {day} and the time is {t}")
+    else:
+        speak("Good Evening! Rushabh, it's {day} and the time is {t}")
+
+    speak("I am your edit. Please tell me how may I help you") 
+
+if __name__ == "__main__":  
+    while True:
+        wishMe()
+        # query = command().lower()
+        query = command().lower()
+        # query = input("Enter your command: ")
+
+
+        if "exit" in query:
+            speak("Goodbye")
+            break
+        else:
+            speak("I am listening...")  
+        print(query)
+
+
+
+# speak("Hello, I am your edit. How can I help you?")
